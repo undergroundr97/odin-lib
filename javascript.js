933beaed5ca3db4451c08762d319ceeb41672b6d
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2024-04-01 13:04:00 -0300

    button and modal showing up

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const myLibrary = [
+    
     {
         title: "Skyward",
         author: "Brandon Sanderson",
@@ -12,7 +13,7 @@ const myLibrary = [
         read: "read",
     }
 ];
-
+let counter = 0;
 
 // Book Constructor
 
@@ -31,7 +32,7 @@ function BookObject(title, author, pages, read) {
 function addBookToLibrary(title,author,pages,read){
     let newBook = new BookObject(title,author,pages,read);
     myLibrary.push(newBook);
-    console.log(myLibrary)
+    appendBook();
 }
 
 
@@ -43,8 +44,62 @@ const bookContainer = document.querySelector('.book-container')
 
 //append Book to DOM
 function appendBook(){
-   myLibrary.forEach(book, index) = function() {
-    const bookContainer = document.createElement('div');
+    // let counter = 0;
+    // mainContainer.innerHTML = '';
+    // mainContainer.append(bookContainer);
+   myLibrary.forEach((book) => {
+    if (myLibrary.indexOf(book) >= counter){
+    const bookCard = document.createElement('div');
+    bookCard.setAttribute('class', 'book-card')
 
+    const titleText = document.createElement('h2');
+    titleText.innerText = book.title;
+    titleText.setAttribute('class', 'book-title');
+    const authorText = document.createElement('h2');
+    authorText.innerText = book.author;
+    const pagesText = document.createElement('p');
+    pagesText.innerText = book.pages;
+    const readText = document.createElement('p');
+     readText.innerHTML = book.read;
+
+    bookCard.append(titleText,authorText,pagesText,readText);
+    bookContainer.appendChild(bookCard);
+
+    counter++;
+    }
    }
-}
+)}
+
+const NEWBOOK = document.createElement('button')
+NEWBOOK.innerHTML = 'ADD NEW BOOK'
+const NEWTITLE = document.querySelector('.title-book')
+NEWTITLE.append(NEWBOOK);
+
+
+// ADDING NEW BOOK TO DOM
+
+const dialog = document.querySelector("dialog");
+NEWBOOK.addEventListener('click', () => {
+    dialog.showModal();
+})
+// const showButton = document.querySelector("dialog + button");
+// const closeButton = document.querySelector("dialog button");
+
+// // "Show the dialog" button opens the dialog modally
+// showButton.addEventListener("click", () => {
+//   dialog.showModal();
+// });
+
+// // "Close" button closes the dialog
+// closeButton.addEventListener("click", () => {
+//   dialog.close();
+// });
+
+// const confirmBtn = dialog.querySelector('#confirmBtn')
+
+// confirmBtn.addEventListener('click', (e) => {
+//     confirmBtn
+// }
+
+appendBook();
+
